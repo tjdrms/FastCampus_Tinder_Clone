d2f1648b6a7e8b17346748febeb98008346ae05a
tjdrms <tjdrms0116>
2022-04-05 22:19:57 +0900

    패스트캠퍼스 실습으로 만든 틴더 클론앱

---
+++ app/src/main/java/com/example/tinderclone/LikeActivity.kt
@@ -0,0 +1,78 @@
+package com.example.tinderclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class LikeActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var userDB: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_like)
+
+        userDB = Firebase.database.reference.child("Users")
+
+        val currentUserDB = userDB.child(getCurrentUserID())
+        currentUserDB.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.child("name").value == null) {
+                    showNameInputPopUp()
+                    return
+                } else {
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun showNameInputPopUp() {
+        val editText = EditText(this)
+
+        AlertDialog.Builder(this)
+            .setTitle("이름을 입력해주세요!")
+            .setView(editText)
+            .setPositiveButton("저장") { _, _ ->
+                if (editText.text.isEmpty()) {
+                    showNameInputPopUp()
+                } else {
+                    saveUserName(editText.text.toString())
+                }
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun saveUserName(name: String) {
+        val userId = getCurrentUserID()
+        val currentUserDB = userDB.child(userId)
+        val user = mutableMapOf<String, Any>()
+        user["userId"] = userId
+        user["name"] = name
+        currentUserDB.updateChildren(user)
+    }
+
+    private fun getCurrentUserID(): String {
+        if(auth.currentUser == null) {
+            Toast.makeText(this, "로그인이 되어있지 않습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        return auth.currentUser?.uid.orEmpty()
+    }
+}
